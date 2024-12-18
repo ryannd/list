1e95c000803e232e6cf858bacd65292b197a0fe8
ryannd <ryan2dimaranan@gmail.com>
2024-12-18 16:56:48 -0600

    :technologist: (root) add commit tools

--- server/src/main/java/com/ryannd/list_api/controller/SearchController.java
+++ server/src/main/java/com/ryannd/list_api/controller/SearchController.java
@@ -1,23 +1,19 @@
 package com.ryannd.list_api.controller;
 
+import com.ryannd.list_api.service.TmdbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ryannd.list_api.service.TmdbService;
-
 @RestController
 @RequestMapping(value = "/search")
 public class SearchController {
-    @Autowired
-    private TmdbService tmdbService;
+    @Autowired private TmdbService tmdbService;
 
     @GetMapping("/movies")
-    public String searchMovies(
-        @RequestParam String query
-    ) {
+    public String searchMovies(@RequestParam String query) {
         return tmdbService.searchMovies(query);
     }
 }
