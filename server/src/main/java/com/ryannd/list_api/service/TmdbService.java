7e5af623cf18bba8e3d25f83aef7bb6cdc463d44
ryannd <ryan2dimaranan@gmail.com>
2024-12-19 13:46:02 -0600

    :sparkles: (server) add search routes, serialize api response

--- server/src/main/java/com/ryannd/list_api/service/TmdbService.java
+++ server/src/main/java/com/ryannd/list_api/service/TmdbService.java
@@ -1,5 +1,6 @@
 package com.ryannd.list_api.service;
 
+import com.ryannd.list_api.domain.TmdbSearchResults;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,13 +25,32 @@ public class TmdbService {
         this.webClient = webClientBuilder.baseUrl("https://api.themoviedb.org/3").build();
     }
 
-    public String searchMovies(String query) {
+    public TmdbSearchResults searchMovies(String query) {
         return getWithAuth()
                 .uri(
                         uriBuilder ->
                                 uriBuilder.path("/search/movie").queryParam("query", query).build())
                 .retrieve()
-                .bodyToMono(String.class)
+                .bodyToMono(TmdbSearchResults.class)
+                .block();
+    }
+
+    public TmdbSearchResults searchShows(String query) {
+        return getWithAuth()
+                .uri(uriBuilder -> uriBuilder.path("/search/tv").queryParam("query", query).build())
+                .retrieve()
+                .bodyToMono(TmdbSearchResults.class)
+                .block();
+    }
+
+    public TmdbSearchResults searchAll(String query) {
+        // TODO: filter people out
+        return getWithAuth()
+                .uri(
+                        uriBuilder ->
+                                uriBuilder.path("/search/multi").queryParam("query", query).build())
+                .retrieve()
+                .bodyToMono(TmdbSearchResults.class)
                 .block();
     }
 }
